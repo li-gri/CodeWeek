67d4234c091992fb4cc6ba25a83afe922a2c3695
Lidia Grigoriev <lidia.grigoriev.ext@iu.org>
2022-10-17 22:32:08 +0200

    updated drunken sailor

--- drunkenSailor.rb
+++ drunkenSailor.rb
@@ -1,140 +1,128 @@
-# Melodie
+# Funktionen
 
-define :part1 do
+define :note_widerholen do |note|
   2.times do
-    play :A
-    sleep 0.5
-    play :A
-    sleep 0.25
-    play :A
-    sleep 0.25
+    play_pattern_timed [note,note,note], [0.5, 0.25, 0.25]
   end
 end
 
+define :muster1 do
+  play_pattern_timed [:A,:B,:C5,:D5], 0.5
+  play_pattern_timed [:C5,:A,:G,:E], 0.5
+  play :D
+  sleep 1
+  play :D
+  sleep 1
+end
 
-define :part2 do
+
+##| Melodie
+
+use_bpm 90
+
+live_loop :melodie do
+  use_synth :bass_foundation
+  
+  note_widerholen :A
+  play_pattern_timed [:A,:D,:F,:A], 0.5
+  note_widerholen :G
+  play_pattern_timed [:G,:C,:E,:G], 0.5
+  note_widerholen :A
+  muster1
+  
+  ##| Hooray Teil
+  
+  play_pattern_timed [:a,:a,:a], [1, 0.75, 0.25]
+  play_pattern_timed [:A,:D,:F,:A], 0.5
+  play_pattern_timed [:g,:g,:g], [1, 0.75, 0.25]
+  play_pattern_timed [:G,:C,:E,:G], 0.5
+  play_pattern_timed [:a,:a,:a], [1, 0.75, 0.25]
+  muster1
+  
+end
+
+live_loop :akkorde do
+  use_synth :pretty_bell
+  
   2.times do
-    play :G
-    sleep 0.5
-    play :G
-    sleep 0.25
-    play :G
-    sleep 0.25
+    play chord(:d, :minor), sustain: 1.5
+    sleep 2
+  end
+  
+  2.times do
+    play chord(:c, :major), sustain: 1.5
+    sleep 2
+  end
+  
+  2.times do
+    play chord(:d, :minor), sustain: 1.5
+    sleep 2
+  end
+  
+  play chord(:c, :major), sustain: 1.5
+  sleep 2
+  
+  3.times do
+    play chord(:d, :minor), sustain: 1.75
+    sleep 2
+  end
+  
+  2.times do
+    play chord(:c, :major), sustain: 1.5
+    sleep 2
+  end
+  
+  2.times do
+    play chord(:d, :minor), sustain: 1.5
+    sleep 2
+  end
+  
+  play chord(:c, :major), sustain: 1.5
+  sleep 2
+  
+  play chord(:d, :minor), sustain: 1.5
+  sleep 2
+end
+
+##| Beats
+
+live_loop :drums1 do
+  sample :drum_heavy_kick
+  sleep 1
+  sample :drum_snare_hard
+  sleep 1
+end
+
+live_loop :drum2 do
+  with_fx :reverb, room: 0.9 do
+    sample :bd_mehackit, amp: 0.4
+    sleep 1.0
   end
 end
 
-use_bpm 100
+live_loop :drum3 do
+  sample :drum_snare_soft, amp: 0.7
+  sleep 1.0
+end
 
-part1
+live_loop :hihat do
+  sample :drum_cymbal_closed
+  sleep 0.25
+  sample :drum_cymbal_pedal
+  sleep 1
+end
 
-play :A
-sleep 0.5
-play :D
-sleep 0.5
-play :F
-sleep 0.5
-play :A
-sleep 0.5
+live_loop :bass do
+  use_synth :fm
+  play :c2, attack: 0, release: 0.25
+  sleep 0.25
+  play :c2, attack: 0, release: 0.3
+  sleep 2
+  play :e2
+  sleep 0.75
+  play :f2
+  sleep 1
+end
 
-part2
 
-play :G
-sleep 0.5
-play :C
-sleep 0.5
-play :E
-sleep 0.5
-play :G
-sleep 0.5
-
-part1
-
-play :A
-sleep 0.5
-play :B
-sleep 0.5
-play :C5
-sleep 0.5
-play :D5
-sleep 0.5
-
-play :C5
-sleep 0.5
-play :A
-sleep 0.5
-play :G
-sleep 0.5
-play :E
-sleep 0.5
-
-play :D
-sleep 1
-play :D
-sleep 1
-
-##| Hooray Part
-
-play :A
-sleep 1
-play :A
-sleep 0.75
-play :A
-sleep 0.15
-
-play :A
-sleep 0.5
-play :D
-sleep 0.5
-play :F
-sleep 0.5
-play :A
-sleep 0.5
-
-play :G
-sleep 1
-play :G
-sleep 0.75
-play :G
-sleep 0.15
-
-play :G
-sleep 0.5
-play :C
-sleep 0.5
-play :E
-sleep 0.5
-play :G
-sleep 0.5
-
-play :A
-sleep 1
-play :A
-sleep 0.75
-play :A
-sleep 0.15
-
-play :A
-sleep 0.5
-play :B
-sleep 0.5
-play :C5
-sleep 0.5
-play :D5
-sleep 0.5
-
-play :C5
-sleep 0.5
-play :A
-sleep 0.5
-play :G
-sleep 0.5
-play :E
-sleep 0.5
-
-play :D
-sleep 1
-play :D
-sleep 1
-
-# Beats
 
